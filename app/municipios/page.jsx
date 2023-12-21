d61e77c039aca889a9a6af8c94ed8990dc9f7b54
scsx <4182349+scsx@users.noreply.github.com>
2023-12-21 05:10:57 +0000

    Tidy

--- app/municipios/page.jsx
+++ app/municipios/page.jsx
@@ -1,12 +1,12 @@
+'use client'
+
 import Link from 'next/link'
 
 const getData = async () => {
   const res = await fetch('https://json.geoapi.pt/municipios')
-
   if (!res.ok) {
     throw new Error('Failed to fetch municipios')
   }
-
   return res.json()
 }
 
@@ -20,19 +20,15 @@ const Municipios = async () => {
   // https://json.geoapi.pt/municipio/Lagoa%20(açores)
 
   return (
-    <div>
+    <div className='municipios'>
       <div className='container'>
+
         <h1>Municípios</h1>
         <ul>
           {municipios &&
             municipios.map((item) => (
               <li key={JSON.stringify(item)} data-key={JSON.stringify(item)}>
-                <Link href={`/municipios/${item}`}>
-                  {item}
-                </Link>
-                {/* <Link href={`/municipios/${encodeURIComponent(item)}`}>
-                  {item}
-                </Link> */}
+                <Link href={`/municipios/${item}`}>{item}</Link>
               </li>
             ))}
         </ul>
