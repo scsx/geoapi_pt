b8296a5fe68c23cba951bc19d2c4ef0a2ee53d3c
scsx <4182349+scsx@users.noreply.github.com>
2023-12-31 21:07:06 +0000

    Remove comments

--- app/municipios/page.jsx
+++ app/municipios/page.jsx
@@ -45,38 +45,6 @@ const Municipios = () => {
 
     return () => (mounted = false)
   }, [])
-  /* 
-  useEffect(() => {
-    fetch('https://json.geoapi.pt/municipios')
-      .then((res) => res.json())
-      .then((data) => {
-        // Sem localeCompare os nomes começados por special chars iam para o fim da array (ex: Vizela, Vouzela, Águeda, ...)
-        const sortedAlph = data.sort(function (a, b) {
-          return a.localeCompare(b)
-        })
-        setMunicipios(sortedAlph)
-        setLoading(false)
-
-        // Obter todas as primeiras letras de cada item, sem acentos
-        let allLetters = sortedAlph.map((mun) => {
-          return mun
-            .charAt(0)
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '')
-        })
-
-        // Remover duplicados
-        allLetters = allLetters.filter(
-          (item, index) => allLetters.indexOf(item) === index
-        )
-
-        // Update state
-        setExistingLetters(allLetters)
-      })
-      .catch((error) => {
-        throw error
-      })
-  }, []) */
 
   return (
     <div className='sitepage sitepage--municipios'>
